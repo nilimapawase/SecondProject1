f846af5aee153f4665e8fa2d3838255ae105fd8f
Nilima Pawase <nilimapawase14@gmail.com>
2026-02-03 14:59:53 +0530

    Initial commit

---
+++ src/main/java/Project2.java
@@ -0,0 +1,14 @@
+
+public class Project2 {
+	public void Display()
+	{
+		System.out.println("Calling Display Function");
+	}
+	
+	public static void main(String[] args)
+	{
+		Project2 p=new Project2();
+		p.Display();
+	}
+
+}
